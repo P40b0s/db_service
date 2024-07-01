e4ecc235ed0711657a7cea9c23b0d3aa48f8db10
P40b0s <uranius666@gmail.com>
2024-07-01 12:05:41 +0300

    forget import POOL

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -2,4 +2,4 @@ mod connection;
 mod operations;
 mod orders;
 pub use operations::{Operations, CountRequest, IdSelector, from_json, to_json, SortingOrder, Selector, QuerySelector};
-pub use connection::new_connection;
+pub use connection::{new_connection, POOL};
