ff970b3762bfdf169e12c11aece5353ad145adea
P40b0s <uranius666@gmail.com>
2024-07-05 10:48:43 +0300

    Забыл экспортировать то что сделал...

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,5 +1,5 @@
 mod connection;
 mod operations;
 mod orders;
-pub use operations::{Operations, CountRequest, IdSelector, from_json, to_json, SortingOrder, Selector, QuerySelector};
+pub use operations::{Operations, SqlOperations, CountRequest, IdSelector, from_json, to_json, SortingOrder, Selector, QuerySelector};
 pub use connection::{new_connection, POOL};
